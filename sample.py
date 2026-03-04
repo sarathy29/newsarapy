2b00cb497ee153e20fd41bdd69ae36e2ca35606a
unknown <sarathymanoj86@gmail.com>
2026-03-04 18:43:30 +0530

    sampledemo

---
+++ sample.py
@@ -0,0 +1,15 @@
+print("Hello, Welcome To Python Class")
+print("...........................")
+username = "sarathy"
+exp = 10
+is_active = True
+value = 12.00
+print("username: ",username)
+print("exp: ",exp)
+print("is_active: ",is_active)
+print("value: ",value)
+print("...........................")
+print(type(username))
+print(type(exp))
+print(type(is_active))
+print(type(value))
